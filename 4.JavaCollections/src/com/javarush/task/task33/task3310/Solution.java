adfbb3d76f9e1b7938c5d30a5afc26f5fa33d6b8
Oleg <olligator58@gmail.com>
2022-09-04 13:30:46 +0300

    Collections Level 06_15

---
+++ 4.JavaCollections/src/com/javarush/task/task33/task3310/Solution.java
@@ -0,0 +1,55 @@
+package com.javarush.task.task33.task3310;
+
+import com.javarush.task.task33.task3310.strategy.HashMapStorageStrategy;
+import com.javarush.task.task33.task3310.strategy.StorageStrategy;
+
+import java.util.Date;
+import java.util.HashSet;
+import java.util.Set;
+
+public class Solution {
+
+    public static Set<Long> getIds(Shortener shortener, Set<String> strings) {
+        Set<Long> result = new HashSet<>();
+        for (String string : strings) {
+            result.add(shortener.getId(string));
+        }
+        return result;
+    }
+
+    public static Set<String> getStrings(Shortener shortener, Set<Long> keys) {
+        Set<String> result = new HashSet<>();
+        for (Long key : keys) {
+            result.add(shortener.getString(key));
+        }
+        return result;
+    }
+
+    public static void testStrategy(StorageStrategy strategy, long elementsNumber) {
+        Helper.printMessage(strategy.getClass().getSimpleName());
+        Set<String> strings = new HashSet<>();
+        for (int i = 0; i < elementsNumber; i++) {
+            strings.add(Helper.generateRandomString());
+        }
+        Shortener shortener = new Shortener(strategy);
+        Date begin = new Date();
+        Set<Long> keys = getIds(shortener, strings);
+        Date end = new Date();
+        long workTime = end.getTime() - begin.getTime();
+        Helper.printMessage(workTime + "");
+        begin = new Date();
+        Set<String> result = getStrings(shortener, keys);
+        end = new Date();
+        workTime = end.getTime() - begin.getTime();
+        Helper.printMessage(workTime + "");
+        if (strings.equals(result)) {
+            Helper.printMessage("Тест пройден.");
+        } else {
+            Helper.printMessage("Тест не пройден.");
+        }
+    }
+
+    public static void main(String[] args) {
+        testStrategy(new HashMapStorageStrategy(), 10000);
+    }
+}
